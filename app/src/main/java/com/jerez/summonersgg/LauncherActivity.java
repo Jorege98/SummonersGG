56bfcd416f38569518cb2b2afa8307a7020fcf07
Jorge <jerez_1998@hotmail.com>
2019-04-29 17:40:06 +0200

    Splash Activity

---
+++ app/src/main/java/com/jerez/summonersgg/LauncherActivity.java
@@ -0,0 +1,18 @@
+package com.jerez.summonersgg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
