583abde3eb99f09664cb0d3e7e0f46f9636f214f
Jorge <jerez_1998@hotmail.com>
2019-04-30 20:19:40 +0200

    Cambios menores al Splash Activity

--- app/src/main/java/com/jerez/summonersgg/LauncherActivity.java
+++ app/src/main/java/com/jerez/summonersgg/LauncherActivity.java
@@ -2,11 +2,13 @@ package com.jerez.summonersgg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+
+import com.merakianalytics.orianna.Orianna;
+import com.merakianalytics.orianna.types.common.Region;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -14,5 +16,15 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        Orianna.setRiotAPIKey("RGAPI-074774e5-41da-4823-9559-5c9f6c5de07e");
+        Orianna.setDefaultRegion(Region.EUROPE_WEST);
+
+        ConnectivityManager cm =(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnected();
+
+        System.out.println(isConnected);
     }
 }
