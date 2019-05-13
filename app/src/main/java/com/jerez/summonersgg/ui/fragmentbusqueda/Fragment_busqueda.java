d6690f3d341fa9d616f4e5c3897fe11ed28adc91
Jorge <jerez_1998@hotmail.com>
2019-05-13 21:57:31 +0200

    0.3
    Implementado settings

--- app/src/main/java/com/jerez/summonersgg/ui/fragmentbusqueda/Fragment_busqueda.java
+++ app/src/main/java/com/jerez/summonersgg/ui/fragmentbusqueda/Fragment_busqueda.java
@@ -2,17 +2,23 @@ package com.jerez.summonersgg.ui.fragmentbusqueda;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.jerez.summonersgg.R;
+
+import java.util.Map;
 
 public class Fragment_busqueda extends Fragment {
 
@@ -24,8 +30,8 @@ public class Fragment_busqueda extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_busqueda, container, false);
     }
 
@@ -33,7 +39,17 @@ public class Fragment_busqueda extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("preferences", Context.MODE_PRIVATE);
+        Map<String, ?> all = preferences.getAll();
+
+        setText(all.toString());
         // TODO: Use the ViewModel
     }
 
+    public void setText(String text){
+
+        TextView textView = getView().findViewById(R.id.message);
+        textView.setText(text);
+    }
 }
