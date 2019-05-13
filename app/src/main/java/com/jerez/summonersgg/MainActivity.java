003add1036478ca637637fadfa1dbce02641cc54
Jorge <jerez_1998@hotmail.com>
2019-05-13 21:57:31 +0200

    1.2
    Implementado settings

--- app/src/main/java/com/jerez/summonersgg/MainActivity.java
+++ app/src/main/java/com/jerez/summonersgg/MainActivity.java
@@ -3,6 +3,8 @@ package com.jerez.summonersgg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.jerez.summonersgg.ui.fragmentbusqueda.Fragment_busqueda;
 
@@ -17,10 +19,43 @@ public class MainActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.StatusBarHome);
 
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, Fragment_busqueda.newInstance())
                     .commitNow();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(false);
+                Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.StatusBarHome);
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, Fragment_busqueda.newInstance())
+                        .commitNow();
+
+                return true;
+            case R.id.settings:
+                Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+                Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.settings_tittle);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, new SettingsFragment())
+                        .commit();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
