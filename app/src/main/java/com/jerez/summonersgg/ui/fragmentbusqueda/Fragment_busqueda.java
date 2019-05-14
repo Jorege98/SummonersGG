c62a456272f497b84c377b85f9e001180f8c040d
Jorge <jerez_1998@hotmail.com>
2019-05-14 20:51:56 +0200

    0.3.1
    Some changed done

--- app/src/main/java/com/jerez/summonersgg/ui/fragmentbusqueda/Fragment_busqueda.java
+++ app/src/main/java/com/jerez/summonersgg/ui/fragmentbusqueda/Fragment_busqueda.java
@@ -2,7 +2,6 @@ package com.jerez.summonersgg.ui.fragmentbusqueda;
 
 import androidx.lifecycle.ViewModelProviders;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -11,14 +10,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+
 import com.jerez.summonersgg.R;
 
-import java.util.Map;
+import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
+
+import java.io.IOException;
+
 
 public class Fragment_busqueda extends Fragment {
 
@@ -31,7 +36,8 @@ public class Fragment_busqueda extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         return inflater.inflate(R.layout.fragment_busqueda, container, false);
     }
 
@@ -40,14 +46,28 @@ public class Fragment_busqueda extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
 
-        SharedPreferences preferences = this.getActivity().getSharedPreferences("preferences", Context.MODE_PRIVATE);
-        Map<String, ?> all = preferences.getAll();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String regionPeference = preferences.getString("region", null);
 
-        setText(all.toString());
+        String[] regions = getResources().getStringArray(R.array.regions);
+
+        for (String region:regions) {
+
+        }
+
+        try {
+            Summoner summoner = mViewModel.loadSummoner();
+            setText(summoner.getName());
+        } catch (RiotApiException e) {
+            e.printStackTrace();
+        }
+
+        //setText(regionPeference);
         // TODO: Use the ViewModel
     }
 
-    public void setText(String text){
+    //metodo de pruebas para set text
+    private void setText(String text){
 
         TextView textView = getView().findViewById(R.id.message);
         textView.setText(text);
--- app/src/main/java/com/jerez/summonersgg/ui/fragmentbusqueda/MainActivityViewModel.java
+++ app/src/main/java/com/jerez/summonersgg/ui/fragmentbusqueda/MainActivityViewModel.java
@@ -2,6 +2,23 @@ package com.jerez.summonersgg.ui.fragmentbusqueda;
 
 import androidx.lifecycle.ViewModel;
 
+import net.rithms.riot.api.ApiConfig;
+import net.rithms.riot.api.RiotApi;
+import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
+import net.rithms.riot.constant.Platform;
+
 public class MainActivityViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+
+
+    public Summoner loadSummoner() throws RiotApiException {
+
+        ApiConfig config = new ApiConfig().setKey("RGAPI-418cec1b-07dd-447e-a2bf-6a93f4eececc");
+        RiotApi api = new RiotApi(config);
+
+        Summoner summoner = api.getSummonerByName(Platform.EUW, "OG xPako");
+
+        return summoner;
+    }
 }
